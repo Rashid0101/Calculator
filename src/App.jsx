ac7c25f877e994a58ca061be7d14732bb6e5217b
Rashid <127450565+Rashid0101@users.noreply.github.com>
2025-02-09 21:00:41 +0200

    dsadaqq

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import { useState } from "react";
 import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
 import "./reset.css";
-// Ensure you have this component
 
 export default function App() {
   const [input, setInput] = useState("");
 
   return (
-    <Router basename="/calculator">
+    <>
       <Nav />
       <Output input={input} />
       <Keyboard input={input} setInput={setInput} />{" "}
-    </Router>
+    </>
   );
 }
 
